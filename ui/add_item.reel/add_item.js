2660dbe3972d5007963175a761c3cd0e7b9e4c50
jonfu86 <noreply>
2014-08-26 00:28:37 +0000

    Add component add_item

---
+++ ui/add_item.reel/add_item.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/add_item.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Add_item
+ * @extends Component
+ */
+exports.Add_item = Component.specialize(/** @lends Add_item# */ {
+    constructor: {
+        value: function Add_item() {
+            this.super();
+        }
+    }
+});
