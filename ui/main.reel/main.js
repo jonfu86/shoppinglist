fe172ea013306d8adcbd3d83a2ae60de26ed56fc
jonfu86 <noreply>
2014-08-28 22:30:26 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -25,7 +25,7 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         value: function (event) {
             this.templateObjects.rangeController.addContent();
             this.templateObjects.rangeController.content[this.templateObjects.rangeController.content.length-1].text = this.templateObjects.newItem.value;
-            console.log(this.templateObjects.rangecontroller.content[0].text);
+            console.log(this.templateObjects.rangeController.content[0].text);
         }
     }
 
