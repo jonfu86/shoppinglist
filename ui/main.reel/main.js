5eec808f5ce39fa20a399fe1b98373d27ab9228a
jonfu86 <noreply>
2014-08-29 00:55:26 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -33,13 +33,14 @@ exports.Main = Component.specialize(/** @lends Main# */ {
     
     handleDestroyButtonAction:{
         value: function (event) {
+           
             this.templateObjects.rangeController.delete(event.detail.get('item'))
         }
     },
     
     handleClearButtonAction:{
         value: function (event) {
-            this.templateObjects.rangeController.delete('item')
+            this.templateObjects.rangeController.clear()
             alert('test')
         }
     }
