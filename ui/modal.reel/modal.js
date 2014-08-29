7291b2e9b55e641bfc1cebfbe13ffe1dcb131006
jonfu86 <noreply>
2014-08-29 20:41:50 +0000

    Add component modal

---
+++ ui/modal.reel/modal.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/modal.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Modal
+ * @extends Component
+ */
+exports.Modal = Component.specialize(/** @lends Modal# */ {
+    constructor: {
+        value: function Modal() {
+            this.super();
+        }
+    }
+});
