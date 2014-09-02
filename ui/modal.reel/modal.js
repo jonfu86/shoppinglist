70c1b70e4cd2654bf4d99b6acf038a8b77ff7fc7
jonfu86 <noreply>
2014-09-02 17:51:09 +0000

    Update component ui/modal.reel/modal.js

--- ui/modal.reel/modal.js
+++ ui/modal.reel/modal.js
@@ -22,7 +22,7 @@ exports.Modal = Component.specialize(/** @lends Modal# */ {
     },
     handleConfirmAction:{
         value: function (event) {
-            this.templateObjects.rangeController.clear();
+            this.templateObjects.main.clearRangeController();
             //this.templateObjects.overlayModal.hide();
         }
     }
