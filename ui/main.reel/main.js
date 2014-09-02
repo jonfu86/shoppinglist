f0f4e54e840545fa82b2539a2bab8913b296b49f
jonfu86 <noreply>
2014-09-02 18:15:14 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -50,7 +50,7 @@ exports.Main = Component.specialize(/** @lends Main# */ {
         }
     },
     
-    handleClearRangeController:{
+    handleclearRangeController:{
         value: function (event) {
             console.log('hi');
             this.templateObjects.rangeController.clear();
